44f373ea35071939088b8b3a158921be5f045415
Hojin Lee <leo.lab001@gmail.com>
2021-11-03 15:54:07 +0900

    Fix typo grid_sampler_3d_cuda

--- aten/src/ATen/native/cuda/GridSampler.cu
+++ aten/src/ATen/native/cuda/GridSampler.cu
@@ -770,11 +770,11 @@ Tensor grid_sampler_3d_cuda(const Tensor& input, const Tensor& grid,
   auto N = input.size(0);
   auto D = grid.size(1);
   auto H = grid.size(2);
-  auto W = grid.size(3);
+auto W = grid.size(3);
   auto output = at::empty({N, input.size(1), D, H, W}, input.options());
   int64_t count = N * D * H * W;
   if (count > 0) {
-    AT_DISPATCH_FLOATING_TYPES_AND_HALF(input.scalar_type(), "grid_sampler_2d_cuda", [&] {
+    AT_DISPATCH_FLOATING_TYPES_AND_HALF(input.scalar_type(), "grid_sampler_3d_cuda", [&] {
       if (canUse32BitIndexMath(input) && canUse32BitIndexMath(grid) &&
           canUse32BitIndexMath(output)) {
         grid_sampler_3d_kernel<scalar_t>
